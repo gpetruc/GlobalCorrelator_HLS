bd1c3724d2a326eb60bb4b9cb65d6bda52d053b5
Giovanni <gpetruc@gmail.com>
2021-03-01 13:43:56 +0100

    Fix Puppi test: load ptErr scales

--- puppi/linpuppi_test.cpp
+++ puppi/linpuppi_test.cpp
@@ -47,7 +47,11 @@ int main() {
                           LINPUPPI_priorNe, LINPUPPI_priorNe_1, LINPUPPI_priorPh, LINPUPPI_priorPh_1,
                           Scales::makePt(LINPUPPI_ptCut), Scales::makePt(LINPUPPI_ptCut_1));
 #endif
-    
+    const float ptErr_edges[PTERR_BINS]  = PTERR_EDGES;
+    const float ptErr_offss[PTERR_BINS]  = PTERR_OFFS;
+    const float ptErr_scales[PTERR_BINS] = PTERR_SCALE;
+    pfEmulator.loadPtErrBins(PTERR_BINS, ptErr_edges, ptErr_scales, ptErr_offss);
+     
     // input TP objects and PV
     PFRegion region;
     TkObj track[NTRACK]; 
