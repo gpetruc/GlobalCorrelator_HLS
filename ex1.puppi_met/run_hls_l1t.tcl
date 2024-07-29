8cf2675f82a455b520a9c95a7013bb74a607a496
Giovanni Petrucciani <gpetruc@gmail.com>
2024-07-29 15:48:44 +0200

    fix URL in run_hls_l1t.tcl

--- ex1.puppi_met/run_hls_l1t.tcl
+++ ex1.puppi_met/run_hls_l1t.tcl
@@ -1,5 +1,5 @@
 #exec wget -q -N https://raw.githubusercontent.com/gpetruc/p2-scouting-clients/master/root/data/Puppi.dump -O data/Puppi.dump
-exec wget -q -N http://gpetrucc.web.cern.ch/gpetrucc/drop/puppi_TTbar_PU200_1orbit.raw -O data/Puppi.dump
+exec wget -q -N http://gpetrucc.web.cern.ch/drop/puppi_TTbar_PU200_1orbit.raw -O data/Puppi.dump
 open_project -reset proj_l1t
 set_top compute_sums_l1t
 add_files src/algo.cc
